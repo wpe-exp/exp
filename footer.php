1055494624ce5a05c6e83a87fa72b340aa4adaa7
MUSUS <musus@MUSUSs-MacBook-Pro.local>
2015-02-22 04:16:50 +0900

    make footer area #20

--- footer.php
+++ footer.php
@@ -8,18 +8,26 @@
  */
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'exp' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'exp' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'exp' ), 'exp', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+      <footer class="l-footer">
+        <div class="l-footer__banner"><img src="/assets/images/page/common/banner.png" alt="banner"></div>
+        <div class="fb-area">
+            <div class="fb-area__inner">Facebook Area</div>
+        </div>
+        <div class="footer-bot">
+            <div class="footer-bot__inner">
+                <div class="footer-bot__logo"><img src="/assets/images/page/common/logo_exp.svg" alt="exp"></div>
+                <ul class="footer-bot__list">
+                <li class="footer-bot__item"><a href="https://twitter.com/WPE34" target="_blank"><img src="/assets/images/page/common/icon_tw.svg" alt="exp twitter"></a></li>
+                <li class="footer-bot__item"><a href="https://www.facebook.com/wpe.org" target="_blank"><img src="/assets/images/page/common/icon_fb.svg" alt="exp facebook"></a></li>
+                <li class="footer-bot__item"><a href="https://github.com/featherplain/exp" target="_blank"><img src="/assets/images/page/common/icon_gh.svg" alt="exp github"></a></li>
+                </ul>
+            </div>
+        </div>
+      </footer>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write("<script src='assets/lib/jquery/jquery.min.js'><\/script>");</script>
+    <script src="/assets/js/bundle.js"></script>
 <?php wp_footer(); ?>
-
 </body>
 </html>
