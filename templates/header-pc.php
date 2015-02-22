e501c408120b1d9175527ed5f3e60e2252deccbf
Yutaro <yutaro@studio-rubbish.com>
2015-02-22 17:05:55 +0900

    support globalNav__subList as drop down #27

--- templates/header-pc.php
+++ templates/header-pc.php
@@ -1,13 +1,11 @@
 <header class="l-header">
 	<div class="l-header__inner">
-		<h1 class="siteLogo"><a href="<?php esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/page/common/icon_exp.svg" alt="<?php bloginfo( 'name' ); ?>"></a></h1>
+		<h1 class="siteLogo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/page/common/icon_exp.svg" alt="<?php bloginfo( 'name' ); ?>"></a></h1>
 		<nav class="globalNav">
 			<ul class="globalNav__list">
 				<li class="globalNav__item globalNav__item--about"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">About</a></li>
 				<li class="globalNav__item globalNav__item--category"><a href="<?php echo esc_url( home_url( '/member/' ) ); ?>">Member</a></li>
-				<li clas="globalNav__item">Categories
-					<ul><?php // echo get_category_sub_menu_item(); ?></ul>
-				</li>
+				<li clas="globalNav__item"><a href="">Categories</a><ul class="globalNav__subList"><?php echo get_category_sub_menu_item(); ?></ul></li>
 			</ul>
 			<?php // global_nav
 			$args = array(
