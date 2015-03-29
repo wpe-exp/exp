3e33c8a88d1f30eeb5b7faeb7647f5cf8ec676ca
Yutaro <yutaro@studio-rubbish.com>
2015-03-29 21:43:50 +0900

    define hljs to single of post page #23

--- content-single.php
+++ content-single.php
@@ -16,18 +16,8 @@
 
 	<header class="post__header">
 		<?php // post thumbnail
-		if ( wp_is_mobile() ) {
-
-			$size = 'thumb640x280';
-			$attr = array( 'class'	=> 'attachment-' . $size . ' post__image' );
-
-		} else {
-
-			$size = 'thumb940x400';
-			$attr = array( 'class'	=> 'attachment-' . $size . ' post__image' );
-
-		}
-		the_post_thumbnail( $size, $attr );
+		$size = ( wp_is_mobile() ) ? 'thumb640x280' : 'thumb940x400';
+		the_post_thumbnail( $size, array( 'class'	=> 'attachment-' . $size . ' post__image' ) );
 		?>
 		<div class="post__meta">
 			<time><?php the_time( get_option( 'date_format' ) ); ?></time><?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?><span><?php echo get_the_author_meta( 'display_name' ); ?></span><?php echo $catHtml; ?>
@@ -38,7 +28,7 @@
 		</div>
 	</header>
 
-	<div class="post__content stylePost">
+	<div id="stylePost" class="post__content stylePost">
 		<?php the_content(); ?>
 	</div>
 	<aside class="widget">
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
 // var $ = require('jquery');
 var foundation = require('foundation');
-var script = require('./app/script.js');
+
+(function($){
+  $(document).ready(function(){
+
+    if(typeof $.prototype.foundation === 'function') {
+      $(document).foundation();
+    }
+
+    if(typeof hljs !== 'undefined') {
+      hljs.initHighlightingOnLoad();
+      $('#stylePost').find('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
+    }
+
+  });
+})(jQuery);
